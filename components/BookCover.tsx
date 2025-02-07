0ab4c04db805ecb174be55ed13ac44898f98ffe9
Karsen H <karsenhansen@gmail.com>
2025-02-07 10:51:46 -0800

    finished home page with dummy data

---
+++ components/BookCover.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import React from 'react'
+import BookCoverSvg from './BookCoverSvg'
+
+type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide"
+
+const variantStyles: Record<BookCoverVariant, string> = {
+    extraSmall: 'book-cover_extra_small',
+    small: 'book-cover_small',
+    medium: 'book-cover_medium',
+    regular: 'book-cover_regular',
+    wide: 'book-cover_wide',
+}
+
+interface BookCoverProps {
+    variant?: BookCoverVariant,
+    className?: string,
+    coverColor: string,
+    coverImageUrl: string
+}
+
+const BookCover = ({className, variant="regular", coverColor='#012B48', coverImageUrl='https://placehold.co/400x600.png' }: BookCoverProps) => {
+  return (
+    <div className={cn('relative transition-all duration-300', variantStyles[variant], className)}>
+        <BookCoverSvg coverColor={coverColor} />
+        <div className='absolute z-10' style={{ left: '12%', width: '87.5%', height: '88%'}}>
+            <Image src={coverImageUrl} alt='Book cover' fill className='rounded-sm object-fill' />
+        </div>
+    </div>
+  )
+}
+
+export default BookCover
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,11 +1,55 @@
+import { Book } from '@/types'
+import Image from 'next/image'
 import React from 'react'
+import StarIcon from '../public/icons/star.svg'
+import { Button } from './ui/button'
+import BookCover from './BookCover'
+import BookIcon from '../public/icons/book.svg'
 
-const BookOverview = () => {
+const BookOverview = ({ title, author, genre, rating, totalCopies, availableCopies, description, colorHash, coverImageUrl }: Book) => {
   return (
     <section className='book-overview'>
         <div className='flex flex-1 flex-col gap-5'>
-            <h1>A long book title</h1>
+            <h1>{title}</h1>
+
+            <div className='book-info'>
+              <p>By <span className='font-semibold text-light-200'>{author}</span></p>
+              <p>Category <span className='font-semibold text-light-200'>{genre}</span></p>
+              <div className='flex flex-row gap-1'>
+                <Image src={StarIcon} alt='star' width={20} height={20} />
+                <p>{rating}</p>
+              </div>
+            </div>
+
+            <div className='book-copies'>
+              <p>Total Books: <span>{totalCopies}</span></p>
+              <p>Available Books: <span>{availableCopies}</span></p>
+            </div>
+
+            <p className='book-description'>{description}</p>
+
+            <Button className='book-overview_btn'>
+              <Image src={BookIcon} alt='book' width={20} height={20} />
+              <p className='font-bebas-neue text-xl text-dark-100'>Borrow</p>
+            </Button>
         </div>
+      <div className="relative flex flex-1 justify-center">
+        <div className="relative">
+          <BookCover
+            className='relative z-40'
+            variant="wide"
+            coverColor={colorHash}
+            coverImageUrl={coverImageUrl}
+          />
+          <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
+            <BookCover
+              variant="wide"
+              coverColor={colorHash}
+              coverImageUrl={coverImageUrl}
+            />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
